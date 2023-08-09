e2cbbff6820e076871d3c0172d9524105aa5366b
Sergei Shcherbak <sshch1987@gmail.com>
2023-08-09 16:22:36 +0300

    Update urls.py

--- baerendreckDjango/baerendreckDjango/urls.py
+++ baerendreckDjango/baerendreckDjango/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('buchstabe'),
+    path('buchstabe', views.buchstabe_liste),
 ]
