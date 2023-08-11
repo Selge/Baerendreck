93e21e25630944a361064010cd02759036daab50
Sergei Shcherbak <sshch1987@gmail.com>
2023-08-11 16:51:36 +0300

    Update urls.py

--- baerendreckDjango/baerendreckDjango/urls.py
+++ baerendreckDjango/baerendreckDjango/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('woerterbuch.urls')),
-    # path('buchstabe/', views.buchstabe_liste),
+    # ,
     # path('buchstabe/<slug:pk>/', views.buchstabe),
 ]
