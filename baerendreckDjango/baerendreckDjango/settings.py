d67b82c4b0886fb21bf97dc6f9aeb0733b6d1650
Sergei Shcherbak <sshch1987@gmail.com>
2023-08-20 23:38:30 +0300

    Update settings.py

--- baerendreckDjango/baerendreckDjango/settings.py
+++ baerendreckDjango/baerendreckDjango/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'baerendreckDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.joinBASE_DIR / 'woerterbuch/../templates']
+        'DIRS': [os.path.join(BASE_DIR / 'woerterbuch/../templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
