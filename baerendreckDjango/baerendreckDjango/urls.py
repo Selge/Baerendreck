8aedef62d545740d749ba20b1ecba7638680ed18
Sergei Shcherbak <sshch1987@gmail.com>
2023-08-23 22:50:46 +0300

    Update urls.py

--- baerendreckDjango/baerendreckDjango/urls.py
+++ baerendreckDjango/baerendreckDjango/urls.py
@@ -11,7 +11,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('woerterbuch.urls', namespace='worterbuch')),
     path('about/', include('about.urls', namespace='about')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
