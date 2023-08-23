96cb7c91060dbd54b2c93f983eed5a5593644751
Sergei Shcherbak <sshch1987@gmail.com>
2023-08-23 15:26:40 +0300

    Update settings.py

--- baerendreckDjango/baerendreckDjango/settings.py
+++ baerendreckDjango/baerendreckDjango/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -67,7 +68,7 @@ WSGI_APPLICATION = 'baerendreckDjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR / 'woerterbuch.sqlite3'),
     }
 }
 
