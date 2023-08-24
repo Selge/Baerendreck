9beca53077da494b78f58a14c2479d27ddc16f3e
Sergei Shcherbak <sshch1987@gmail.com>
2023-08-24 15:14:03 +0300

    Update urls.py

--- baerendreckDjango/baerendreckDjango/urls.py
+++ baerendreckDjango/baerendreckDjango/urls.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('woerterbuch.urls')),
+    path('', include('woerterbuch.urls', namespace='worterbuch')),
+    path('about/', include('about.urls', namespace='about')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
