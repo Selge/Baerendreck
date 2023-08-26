f79560e1eb6a017a3f018aab999e20db78649b18
Sergei Shcherbak <sshch1987@gmail.com>
2023-08-27 00:17:18 +0300

    Aktualisiert

--- baerendreckDjango/woerterbuch/views.py
+++ baerendreckDjango/woerterbuch/views.py
@@ -3,16 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'woerterbuch/baerendreck.html')
+    return render(request, 'woerterbuch/../templates/baerendreck.html')
 
 
 def buchstabe(request):
-    return render(request, 'woerterbuch/Wörtebuch.html')
+    return render(request, 'woerterbuch/../templates/Wörtebuch.html')
 
 
 def buchstabe_liste(request, pk):
-    return render(request, 'woerterbuch/Buchstabe.html', {'buchstabe': get_object_or_404(Buchstabe, pk=buchstabe)})
+    return render(request, 'woerterbuch/../templates/Buchstabe.html', {'buchstabe': get_object_or_404(Buchstabe, pk=buchstabe)})
 
 
 def page_not_found(request):
-    return render(request, 'woerterbuch/404.html')
+    return render(request, 'woerterbuch/../templates/404.html')
