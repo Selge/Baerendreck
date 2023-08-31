e1380779242ef0230fd5d13433c4a8222d50fb75
Sergei Shcherbak <sshch1987@gmail.com>
2023-08-31 15:48:59 +0300

    Aktualisiert

--- baerendreckDjango/woerterbuch/urls.py
+++ baerendreckDjango/woerterbuch/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'woerterbuch'
 
 urlpatterns = [
     path('', views.index),
-    path('buchstabe/', views.buchstabe_liste),
-    path('buchstabe/<slug:pk>/', views.buchstabe),
+    # path('buchstabe/', views.buchstabe_liste),
+    # path('buchstabe/<slug:pk>/', views.buchstabe),
 ]
--- baerendreckDjango/woerterbuch/views.py
+++ baerendreckDjango/woerterbuch/views.py
@@ -3,16 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'woerterbuch/../templates/baerendreck.html')
+    return render(request, 'baerendreck.html')
 
 
 def buchstabe(request):
-    return render(request, 'woerterbuch/../templates/Wörtebuch.html')
+    return render(request, 'Wörtebuch.html')
 
 
-def buchstabe_liste(request, pk):
-    return render(request, 'woerterbuch/../templates/Buchstabe.html', {'buchstabe': get_object_or_404(Buchstabe, pk=buchstabe)})
+# def buchstabe_liste(request, pk):
+#    return render(request, 'woerterbuch/../templates/Buchstabe.html', {'buchstabe': get_object_or_404(Buchstabe, pk=buchstabe)})
 
 
-def page_not_found(request):
-    return render(request, 'woerterbuch/../templates/404.html')
+# def page_not_found(request):
+#    return render(request, 'woerterbuch/../templates/404.html')
