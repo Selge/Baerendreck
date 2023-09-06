7035fdc97f0bc943e31827caeb1555a24b1aae5c
Sergei Shcherbak <sshch1987@gmail.com>
2023-09-06 18:55:20 +0300

    Update models.py

--- baerendreckDjango/woerterbuch/models.py
+++ baerendreckDjango/woerterbuch/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Buchstabe(models.Model):
+    ...
+
+
+class Wort(models.Model):
+    ...
