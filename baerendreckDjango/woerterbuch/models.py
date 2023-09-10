2fd70fe1eab97b46a99681fa416e84616f2ed39f
Sergei Shcherbak <sshch1987@gmail.com>
2023-09-10 18:51:06 +0300

    aktualisiert

--- baerendreckDjango/woerterbuch/models.py
+++ baerendreckDjango/woerterbuch/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
 
 class Dialekt(models.Model):
-    title = models.CharField("Dialekt", max_length=200)
-    slug = models.SlugField("Slug", unique=True)
-    description = models.TextField("Beschreibung")
+    title = models.TextField("Dialekt", max_length=200)
+    slug = models.SlugField("Slug", unique=True, default='Deutsch')
+    description = models.TextField("Beschreibung", max_length=500, blank=True, null=True)
 
     class Meta:
         verbose_name_plural = 'Dialekte'
@@ -15,7 +14,7 @@ class Dialekt(models.Model):
 
 
 class Buchstabe(models.Model):
-    letter = models.CharField("Buchstabe", max_length=1)
+    letter = models.TextField("Buchstabe", max_length=1)
     dialect = models.ForeignKey(
         Dialekt,
         verbose_name="Dialekt",
@@ -26,10 +25,39 @@ class Buchstabe(models.Model):
         help_text='Bitte, wählen Sie eine Dialekte aus'
     )
 
+    class Meta:
+        verbose_name_plural = 'Buchstaben'
+
     def __str__(self):
         return self.letter
 
 
 class Wort(models.Model):
+    text = models.TextField('Wort', max_length=300)
+    uebersetzung = models.TextField('Deutsch', max_length=900)
+    translation = models.TextField('Englisch', max_length=900)
+
+    dialect = models.ForeignKey(
+        Dialekt,
+        verbose_name="Dialekt",
+        on_delete=models.CASCADE,
+        related_name='Dialekt',
+        help_text='Bitte, wählen Sie eine Dialekte aus',
+        blank=True,
+        null=True,
+    )
+    letter = models.ForeignKey(
+        Buchstabe,
+        verbose_name="Buchstabe",
+        on_delete=models.CASCADE,
+        related_name='Buchstabe',
+        help_text='Bitte, wählen Sie eine Buchstabe aus',
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name_plural = 'Wörter'
+
     def __str__(self):
         return self.text
