218f41486085944ba0a43cf46e821e973399aec8
Sergei Shcherbak <sshch1987@gmail.com>
2023-09-10 15:58:52 +0300

    Update models.py

--- baerendreckDjango/woerterbuch/models.py
+++ baerendreckDjango/woerterbuch/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth import get_user_model
 
 
 class Dialekt(models.Model):
-    title = models.CharField("Название", max_length=200)
+    title = models.CharField("Dialekt", max_length=200)
+    
+
 
 
 class Buchstabe(models.Model):
